ef4275903be9a02df5d488da9fbf231ee1b1b642
RipSNake <seba.guzman13@gmail.com>
2021-07-09 15:31:28 -0300

    added Alert's text constantes

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,12 +18,12 @@ export const GET = 'get';
 export const POST = 'post';
 export const PUT = 'put';
 export const DELETE = 'delete';
-
+// Superhero API
 export const API_URL = `https://superheroapi.com/api.php`;
 export const API_TOKEN = '10160265755538455';
-
+// login server
 export const LOGIN_URL = 'http://challenge-react.alkemy.org';
-
+// routes
 export const POWERSTATS = '/powerstats';
 export const BIOGRAPHY = '/biography';
 export const APPEARANCE = '/appearance';
@@ -31,11 +31,33 @@ export const WORK = '/work';
 export const CONNECTIONS = '/connections';
 export const IMAGE = '/image';
 export const SEARCH = '/search';
-
+// buttons texts
 export const ACCEPT_BTN = 'Accept';
 export const DELETE_BTN = 'Delete';
 export const DETAILS_BTN = 'Details';
 export const ADD_BTN = 'Add';
 export const BACK_BTN = 'Back';
-
+// HOmeScreen Strings
 export const AVG = 'Average';
+
+// Alert's Strings
+export const alertErrorStr = {
+	title: 'Error',
+	text: 'No se pudieron obtener los datos!'
+};
+
+export const alertInfoStr = {
+	title: 'Alerta',
+	text: 'Pudiste loguearte. Serás redirigido al Inicio'
+}
+
+export const alertSuccesStr = {
+	title: 'Exito',
+	text: 'Accion realizada con exito'
+}
+
+export const alertConfirmStr = {
+	title: 'Estas seguro?',
+	text: 'No vas a poder revertir esta accion...',
+	confirmBtn: 'Si'
+}
